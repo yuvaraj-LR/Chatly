4b75b7943b06044ae98e36d59e55702f161b1bcc
yuvaraj-LR <yuvarajrama121@gmail.com>
2025-01-15 13:57:28 +0530

    Profile page UI Setup

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,11 +1,13 @@
 import Logo from "./logo/Logo.svg";
 import Google from "./login/Google.svg";
 import Facebook from "./login/Facebook.svg";
+import UserIcon from "./login/user_icon.svg";
 
 const assest = {
     Logo,
     Google,
-    Facebook
+    Facebook,
+    UserIcon
 }
 
 export default assest;
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Profile.css";
+import assest from '../../assets/assets';
 
 const Profile = () => {
+
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="profile_wrapper">
+      <div className="layout">
+      </div>
+      <div className="flex profile_container">
+        <section className="signin-container">
+          <div className="signin-card">
+            <h1 className="heading">Profile Details</h1>
+            <form className="signin-form">
+              <div className="form-group profile_grp">
+                <label htmlFor="profile"><img src={assest.UserIcon} alt="userIcon" /><p className='profile_desc'>Upload your Profile Photo</p></label>
+                <input
+                  id="profile"
+                  type="file"
+                  name="profile"
+                  className={`form-input hidden`}
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="Your Name"
+                  className={`form-input`}
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="about">About</label>
+
+                <textarea className='form-input' id='about' name='about' placeholder='Your about' rows="4" cols="55">
+                </textarea>
+              </div>
+              <button type="submit" className="submit-btn">Save</button>
+            </form>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
